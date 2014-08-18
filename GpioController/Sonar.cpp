1976a1466b223a582b536c61f59ebfe3bf851501
joshua <joshualilly@me.com>
2014-08-17 21:12:14 -0400

    added pins that will be used.

--- GpioController/Sonar.cpp
+++ GpioController/Sonar.cpp
@@ -2,7 +2,10 @@
 
 int main()
 {
-	USonic* u = new USonic("2", "25");
+	//USOnic(in, out)
+	//in voltage must be pulled down to 3.3 voltes
+	//vout = vin * R2/(R1+R2) to calculate needed ressistance
+	USonic* u = new USonic("24", "23");
 	u->sendSignal();
 	u->receiveSignal();
 	delete u;
